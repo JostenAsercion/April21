28abeda4c2b9cd058107b5550a8a6894d926483f
Joa <jostenasercion@gmail.com>
2021-04-06 12:54:04 -0400

    Added first section of Day 1 assignment

---
+++ Java_Week1/Assignment_DayOne/com/ss/jb/one/AssignmentOne.java
@@ -0,0 +1,20 @@
+/**
+ * Base package for java basics
+ */
+package com.ss.jb.one;
+
+/**
+ * Assignment 1 Day 1
+ * Uses the Patterns class to print various patterns
+ * 
+ * @author Joa
+ *
+ */
+public class AssignmentOne {
+
+	public static void main(String[] args) {
+		Patterns pattern = new Patterns();
+		pattern.printPatterns();
+	}
+	
+}
---
+++ Java_Week1/Assignment_DayOne/com/ss/jb/one/Patterns.java
@@ -0,0 +1,115 @@
+/**
+ * 
+ */
+package com.ss.jb.one;
+
+/**
+ * Class for printing various patterns for assignment 1
+ * 
+ * @author Joa
+ *
+ */
+public class Patterns {
+
+	/**
+	 * Prints pattern one
+	 */
+	private void patternOne() {
+		String currLine = "";
+		String lastLine = "";
+		for (int i = 0; i < 4; i++) {
+			currLine = currLine + "*";
+			lastLine = lastLine + "..";
+			System.out.println(currLine);
+		}
+		System.out.println(lastLine + ".\n");
+	}
+
+	/**
+	 * Prints pattern two
+	 */
+	private void patternTwo() {
+		String firstLine = "";
+		String currLine = "";
+
+		for (int i = 0; i < 4; i++) {
+			currLine = currLine + "*";
+			firstLine = firstLine + "..";
+		}
+
+		System.out.println(firstLine + ".");
+
+		for (int i = 4; i > 0; i--) {
+			currLine = currLine.substring(0, i);
+			System.out.println(currLine);
+		}
+		System.out.println();
+	}
+
+	/**
+	 * Prints pattern three
+	 */
+	private void patternThree() {
+		String middle = "*";
+		String asterisks = "";
+		String space = "     ";
+		String lastLine = "";
+
+		for (int i = 0; i < 4; i++) {
+			System.out.println(space + asterisks + middle + asterisks + space);
+			space = space.substring(0, space.length() - 1);
+			asterisks = asterisks + "*";
+			lastLine = lastLine + ".";
+		}
+		System.out.println(lastLine + "..." + lastLine + "\n");
+	}
+
+	/**
+	 * Prints pattern four
+	 */
+	private void patternFour() {
+		String firstLine = "";
+		String middle = "*";
+		String asterisks = "***";
+		String space = "  ";
+
+		for (int i = 0; i < 5; i++) {
+			firstLine = firstLine + ".";
+		}
+
+		System.out.println(firstLine + "." + firstLine);
+		System.out.println(space + asterisks + middle + asterisks + space);
+
+		for (int i = 0; i < 3; i++) {
+			space = space + " ";
+			asterisks = asterisks.substring(0, asterisks.length() - 1);
+			System.out.println(space + asterisks + middle + asterisks + space);
+		}
+	}
+
+	/**
+	 * Prints each pattern with number to identify
+	 */
+	public void printPatterns() {
+		for (int i = 1; i <= 4; i++) {
+			System.out.println(i + ")");
+
+			switch (i) {
+			case 1:
+				patternOne();
+				break;
+			case 2:
+				patternTwo();
+				break;
+			case 3:
+				patternThree();
+				break;
+			case 4:
+				patternFour();
+				break;
+			default:
+				System.out.println("Error. Out of bounds.\n");
+			}
+		}
+	}
+}
